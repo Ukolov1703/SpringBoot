cca5325fb532eda5612e98b59019d3ab42abda07
Ukolov1703 <Nissan_Skyline10>
2022-10-02 12:45:48 +0700

    dz4

--- src/main/java/com/geekbrains/service/CartService.java
+++ src/main/java/com/geekbrains/service/CartService.java
@@ -1,4 +1,27 @@
 package com.geekbrains.service;
 
-public class CartService {
+import com.geekbrains.persistence.Cart;
+import com.geekbrains.persistence.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface CartService {
+
+    Cart getNewCart();
+
+    void addProduct(Cart cart, Product product, Integer quantity);
+    void addProduct(Cart cart, Long prodId, Integer quantity);
+
+
+    BigDecimal getSum(Cart cart);
+
+    Integer getItemsAmount(Cart cart);
+
+    void printCart(Cart cart);
+
+    int getProductQuantity(Cart cart, Product product);
+    int getProductQuantity(Cart cart, Long prodId);
+
+    List<Product> getCartListSorted(Cart cart);
 }
--- src/main/java/com/geekbrains/service/ProductService.java
+++ src/main/java/com/geekbrains/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.geekbrains.service;
 
-public class ProductService {
+import com.geekbrains.persistence.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProductList();
+
+    void saveOrUpdate(Product product);
+
+    Product getProductById(Long id);
+
+    void deleteById(Long id);
+
 }
